93ae68fac31f23fd507d4c428295c09073d5b8b0
Kyle Hornberg <kyle.hornberg@gmail.com>
2013-10-29 15:41:09 -0500

    Front end data collection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,7 +16,7 @@ require.config({
         bootstrapDatepicker: '../bower_components/bootstrap-datepicker/js/bootstrap-datepicker',
         bible: '../bower_components/bible.math.js/bible',
         bibleReference: '../bower_components/bible.math.js/bible.reference',
-        bibleMath: '../bower_components/bible.math.js/bible.math'
+        bibleMath: '../bower_components/bible.math.js/bible.math',
     },
     shim: {
         bootstrapAffix: {
@@ -73,14 +73,49 @@ require.config({
 require(['app', 'jquery', 'bibleMath', 'bootstrapDatepicker'], function (app, $) {
     'use strict';
     // use app here
+    // 
+    // tests
+    console.groupCollapsed('App tests');
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
     console.log(bible);
     var ref = bible.parseReference("rom 1:3");
     console.log(bible.add(ref, 10).toString());
+    console.groupEnd();
 
-    $('#calendar-start').datepicker({});
-    $('#calendar-end').datepicker({});
+    // calendars
+    var calDefaultOptions = {
+        "todayHighlight":true,
+        "startDate": new Date()
+    }
+    $('#calendar-start').datepicker(calDefaultOptions);
+    $('#calendar-end').datepicker(calDefaultOptions);
+
+    // the end cannot be before the start
+    $('#calendar-start').change(function() {
+        $('#calendar-end').datepicker('setStartDate', $('#calendar-start').datepicker('getDate'));
+    })
+
+    // the start cannot be after the end
+    $('#calendar-end').change(function() {
+        $('#calendar-start').datepicker('setEndDate', $('#calendar-end').datepicker('getDate'));
+    })
+
+    // disable days
+    $('#skip-checkboxes').change(function () {
+
+        
+        $('#calendar-start').datepicker('setDaysOfWeekDisabled', getSkippedDays());
+        $('#calendar-end').datepicker('setDaysOfWeekDisabled', getSkippedDays());
+    })
+
+    function getSkippedDays () {
+        var opts = [];
+        $('#skip-checkboxes input:checked').each(function (i, input) { 
+            opts.push(input.value); 
+        })
+        return opts;
+    }
 
 
     // Load plans
@@ -91,6 +126,7 @@ require(['app', 'jquery', 'bibleMath', 'bootstrapDatepicker'], function (app, $)
     .done(function(data) {
         var plans = [];
 
+        console.groupCollapsed('Plans');
         console.log("success");
         $(data).find("a:contains(.json)").each(function() {
             plans.push($(this).attr("title"));
@@ -99,20 +135,68 @@ require(['app', 'jquery', 'bibleMath', 'bootstrapDatepicker'], function (app, $)
         console.log(plans);
 
         for (var i = 0; i < plans.length; i++) {
-            var planName = plans[i].split(".");
-            var plan = '<a href="" class="list-group-item">' + planName[0] + '</a>';
+            var planName = plans[i].split("."); // TODO  parse json, get name
+            var plan = '<a href="#" class="list-group-item" name="' + plans[i] + '">' + planName[0] + '</a>';
             console.log(plan);
             $('#sequence').append(plan);
         }
+        console.groupEnd();
+        $('.list-group-item').click(function () { 
+            $('.list-group-item').each(function () {
+                $(this).removeClass('active');
+            })
+
+            $(this).addClass('active');
+        });
+
+
     })
     .fail(function() {
         console.log("error");
         var plan = '<a href="" class="list-group-item">Error Loading Plans</a>';
         $('#sequence').append(plan);
     });
-
-    function addPlans (plans) {
-           
-    }
     
+    // get data
+    define('getData', [], function () {
+        'use strict';
+
+        return function () {
+                var data = [];
+                // sequence
+                data['sequence'] = $('.list-group-item.active').attr('name');
+
+                // start
+                data['start'] = $('#calendar-start').datepicker('getDate');
+
+                // end
+                data['end'] = $('#calendar-end').datepicker('getDate');
+
+                // days to skip
+            //     var opts = [];
+            // $('#skip-checkboxes input:checked').each(function (i, input) { 
+            //     opts.push(input.value); 
+            // })
+                data['skip'] = getSkippedDays();
+
+                // amount
+                data['amount'] = $('#amount').val();
+
+                // type
+                data['type'] = $(':radio:checked').attr('id');
+
+                console.info(data);
+                return data;
+        };
+    });
+
+    // create plan
+    $('#create').click(function(event) {
+        require(['getData', 'plan'], function (getData, plan) {
+            var data = getData();
+        });
+    });
 });
+
+
+
--- app/scripts/plan.js
+++ app/scripts/plan.js
@@ -1,85 +1,89 @@
-define("plan", ["bibleMath"], function(bibleBook, sequence, versesPerDay, remainder, bookRemainder, sequenceKey, results) {
-        
+define(["bibleMath"], function() {
+
     'use strict';
+
+    return function(bibleBook, sequence, versesPerDay, remainder, bookRemainder, sequenceKey, results) {
+               
+        var chapterVerses = bibleBook[sequence[sequenceKey] - 1]; //minus 1 for 0 indexed array
+        var chapter = sequence[sequenceKey]; //sequence must use real chapters
+
+        var beginVerse = (remainder <= 0) ? 1 : chapterVerses - Math.abs(remainder) + 1;
+
+        /**
+         * Read to the end of the chapter unless:
+         * 1) a new book is being read as shown by a (-)negative remainder or a bookRemainder of 0
+         * 2) reaminder in book to be read (bookRemainder) plus the verses/day to be read is less than the verses in the chapter
+         * 3) reaminder in book to be read (bookRemainder) plus the remaining verses to be read is less than the verses in the chapter
+        */
+        var endVerse = null;
         
-    var chapterVerses = bibleBook[sequence[sequenceKey] - 1]; //minus 1 for 0 indexed array
-    var chapter = sequence[sequenceKey]; //sequence must use real chapters
-
-    var beginVerse = (remainder <= 0) ? 1 : chapterVerses - Math.abs(remainder) + 1;
-
-    /**
-     * Read to the end of the chapter unless:
-     * 1) a new book is being read as shown by a (-)negative remainder or a bookRemainder of 0
-     * 2) reaminder in book to be read (bookRemainder) plus the verses/day to be read is less than the verses in the chapter
-     * 3) reaminder in book to be read (bookRemainder) plus the remaining verses to be read is less than the verses in the chapter
-    */
-    var endVerse = null;
-    
-    // 1
-    if (remainder < 0) {
-        endVerse = Math.abs(remainder);
-    }
-    else if (bookRemainder === 0) {
-        endVerse = (remainder === 0) ? versesPerDay : Math.abs(remainder);
-    }
-    // Correction for bookRemainder problem
-    else if (beginVerse === chapterVerses) {
-        endVerse = chapterVerses;
-    }
-    // 3
-    else if (chapterVerses - bookRemainder + versesPerDay < chapterVerses) {
-        endVerse = chapterVerses - bookRemainder + versesPerDay;
-    }
-    else {
-        endVerse = chapterVerses ;
-    }
-    
-    // Only if a reader is reading 1 verse a day
-    if (versesPerDay === 1) {
-        endVerse = beginVerse;
-    }
-
-    var beginReference = {'chapter': chapter, 'verse': beginVerse};
-    var endReference = {'chapter': chapter, 'verse': endVerse};
-
-    //remainder
-    if(versesPerDay <= chapterVerses) { //less than the chapter is read
-        if (remainder === 0) {
-            remainder = chapterVerses - versesPerDay;
+        // 1
+        if (remainder < 0) {
+            endVerse = Math.abs(remainder);
         }
         else if (bookRemainder === 0) {
-            remainder = chapterVerses - Math.abs(remainder);
+            endVerse = (remainder === 0) ? versesPerDay : Math.abs(remainder);
+        }
+        // Correction for bookRemainder problem
+        else if (beginVerse === chapterVerses) {
+            endVerse = chapterVerses;
+        }
+        // 3
+        else if (chapterVerses - bookRemainder + versesPerDay < chapterVerses) {
+            endVerse = chapterVerses - bookRemainder + versesPerDay;
         }
         else {
-            // remainder = (remainder - versesPerDay > 0) ? remainder - versesPerDay : 0;
-            remainder = remainder - versesPerDay;
+            endVerse = chapterVerses ;
         }
-    }
-    else {
-        // more than than the chapter is read
-        remainder = versesPerDay - chapterVerses;
-        sequenceKey++;
-    }
+        
+        // Only if a reader is reading 1 verse a day
+        if (versesPerDay === 1) {
+            endVerse = beginVerse;
+        }
 
-    // TODO not calculating correct book remainder on 4:26 with remainder of 1
-    bookRemainder = (bookRemainder !== 0) ? chapterVerses - Math.abs(remainder) : Math.abs(remainder);
+        var beginReference = {'chapter': chapter, 'verse': beginVerse};
+        var endReference = {'chapter': chapter, 'verse': endVerse};
 
-    if (remainder <= 0) {
-        sequenceKey++;
-        bookRemainder = 0;
-    }
+        //remainder
+        if(versesPerDay <= chapterVerses) { //less than the chapter is read
+            if (remainder === 0) {
+                remainder = chapterVerses - versesPerDay;
+            }
+            else if (bookRemainder === 0) {
+                remainder = chapterVerses - Math.abs(remainder);
+            }
+            else {
+                // remainder = (remainder - versesPerDay > 0) ? remainder - versesPerDay : 0;
+                remainder = remainder - versesPerDay;
+            }
+        }
+        else {
+            // more than than the chapter is read
+            remainder = versesPerDay - chapterVerses;
+            sequenceKey++;
+        }
 
-    if (typeof results === 'undefined' ) {
-        results = new Array();
-    }
+        // TODO not calculating correct book remainder on 4:26 with remainder of 1
+        bookRemainder = (bookRemainder !== 0) ? chapterVerses - Math.abs(remainder) : Math.abs(remainder);
 
-    results.push({'day': results.length, 'start': beginReference, 'end': endReference});
+        if (remainder <= 0) {
+            sequenceKey++;
+            bookRemainder = 0;
+        }
 
-    if (sequence.length !== sequenceKey) {
-        plan(bibleBook, sequence, versesPerDay, remainder, bookRemainder, sequenceKey, results);
-    }
+        if (typeof results === 'undefined' ) {
+            results = new Array();
+        }
 
-    return results;
+        results.push({'day': results.length, 'start': beginReference, 'end': endReference});
+
+        if (sequence.length !== sequenceKey) {
+            plan(bibleBook, sequence, versesPerDay, remainder, bookRemainder, sequenceKey, results);
+        }
+
+        return results;
+
+    };
 });
 
 //sdg
