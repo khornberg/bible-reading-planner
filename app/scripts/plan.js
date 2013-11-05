34b16926556393e7a41598e39f873c93ce380c34
Kyle Hornberg <kyle.hornberg@gmail.com>
2013-11-05 14:06:59 -0600

    Whole chapters accounted for.

--- app/scripts/plan.js
+++ app/scripts/plan.js
@@ -75,13 +75,15 @@ console.info("Data " + JSON.stringify(data));
             if(type === 'specified') {
                 return this.createSpecifiedPlan(sequence, amount);
             }
-            if(type === 'chapters') {
-                return this.createChaptersPlan(sequence, Number(amount));
-            }
+            // if(type === 'chapters') {
+            //     return this.createChaptersPlan(sequence, Number(amount));
+            // }
         },
 
         'createSpecifiedPlan': function (sequence, amount) {
             var items = Math.floor(sequence.data.length / this.duration.length);
+            // If duration is longer than the sequence, adjust to ouput everything
+                items = (items === 0) ? 1 : items;
             var mod = sequence.data.length % this.duration.length;
             var results = [];
             var sequenceKey = 0;
@@ -111,7 +113,8 @@ console.info("Data " + JSON.stringify(data));
 
             // partial sequence
             if (amount === 'partial') {
-                for (var i = 0; i < this.duration.length; i++) {
+                var length = (this.duration.length < sequence.data.length) ? this.duration.length : sequence.data.length;
+                for (var i = 0; i < length; i++) {
                     results.push({'day': this.duration[i].toString(), 'refs': [bible.parseReference(sequence.data[i]).toString()]});
                 }
                     
@@ -133,6 +136,7 @@ console.info("Data " + JSON.stringify(data));
             while(sequenceKey < sequence.data.length) {
                 if (ref === undefined) {
                     var ref = bible.parseReference(sequence.data[sequenceKey]);
+                    console.info(ref.toString());
                 }
 
                 if (ref.isValid()) {
@@ -142,7 +146,7 @@ console.info("Data " + JSON.stringify(data));
                     // amount
                     var amt = (partialReferenceString !== '') ? amount - tmpDistance : amount;
                     if(distance >= amt) { 
-                        var startRef = bible.Reference(ref.bookIndex, ref.chapter1, ref.verse1); // start ref
+                        var startRef = bible.Reference(ref.bookIndex, ref.chapter1, ((ref.verse1 === -1) ? 1 : ref.verse1)); // start ref
                         var startRefString = startRef.toString();
                         var endRef = bible.add(startRef, amt - 1).toString(); // end ref
 
@@ -151,7 +155,7 @@ console.info("Data " + JSON.stringify(data));
                         bible.add(ref, amt);
                         partialReferenceString = '';
                     }
-                    else { 
+                    else {
                         partialReferenceString = (partialReferenceString === '') ? ref.toString() : partialReferenceString + '; ' + ref.toString();
                         ref = undefined;
                         sequenceKey++;
@@ -177,75 +181,16 @@ console.info("Data " + JSON.stringify(data));
                 if (refs[i] !== undefined) {
                     results.push({'day': this.duration[i], 'refs': [refs[i]]});
                 }
-            }                         
+            }
 
             return results;
         },
 
-        'createChaptersPlan': function (sequence, amount) {
+        // not implemented yet
+        // 'createChaptersPlan': function (sequence, amount) {
             
-            var seq = ['Gen 1:1-2:25', 'Matt 1:1-2:12'];
-            var amount = 50;
-            var seqKey = 0;
-            var ref = undefined;
-            var partialReferenceString = '';
-            var remRef = undefined;
-            var results = [];
-
-            while(seqKey < seq.length) {
-                if (ref === undefined) {
-                    ref = bible.parseReference(seq[seqKey]);
-                }
-                
-                console.log('Ref: ' + ref.toString());
-                
-                var distance = (ref !== undefined) ? bible.distance(ref).verses : undefined;
-                var tmpDistance = (partialReferenceString !== '') ? bible.distance(bible.parseReference(partialReferenceString)).verses : '';
-
-                if (partialReferenceString !== '') {
-                    if(distance >= amount - tmpDistance) { 
-                        var startRef = bible.Reference(ref.bookIndex, ref.chapter1, ref.verse1); // start ref
-                        var startRefString = startRef.toString();
-                        var endRef = bible.add(startRef, amount - tmpDistance - 1).toString(); // end ref
-
-                        results.push(partialReferenceString + '; ' + startRefString + '-' + endRef);
-                        bible.add(ref, amount - tmpDistance);
-                        partialReferenceString = '';
-                    }
-                    else { 
-                        partialReferenceString = (partialReferenceString === '') ? ref.toString() : partialReferenceString + '; ' + ref.toString();
-                        ref = undefined;
-                        seqKey++;
-                    }
-                }
-                else {
-                    if(distance >= amount) { 
-                        var startRef = bible.Reference(ref.bookIndex, ref.chapter1, ref.verse1); // start ref
-                        var startRefString = startRef.toString();
-                        var endRef = bible.add(startRef, amount - 1).toString(); // end ref
-
-                        results.push(startRefString + '-' + endRef);
-                        bible.add(ref, amount);
-                        partialReferenceString = '';
-                    }
-                    else { 
-                        partialReferenceString = (partialReferenceString === '') ? ref.toString() : partialReferenceString + '; ' + ref.toString();
-                        ref = undefined;
-                        seqKey++;
-                    }
-                }
-                
-            }
-
-            if (partialReferenceString !== '') {
-                results.push(partialReferenceString);
-            }
-
-            console.info(results);
-            console.info(partialReferenceString);
-
-            return {'day': this.duration[0], 'refs': ['Unavailable']};
-        },
+        //     return;
+        // },
 
         'load': function (sequence) {
             sequence = (sequence === undefined) ? this.sequenceName : sequence;
