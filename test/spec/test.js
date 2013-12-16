2d756cd8c20c9b8f719199fc9c5b1dbb4ce30bf4
Kyle Hornberg <kyle.hornberg@gmail.com>
2013-12-16 11:15:25 -0600

    add tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,19 @@
-/*global describe, it */
-'use strict';
-(function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+/*global describe, it, assert */
+mocha.setup('bdd');
+var assert = chai.assert;
 
+(function () {
+	'use strict';
+    describe('App loaded', function () {
+        describe('Are dependencies available?', function () {
+            it('should have jQuery, moment, twix, time, bible.math, Spinner, and ics available', function () {
+                assert.ok($().jquery, 'jQuery not loaded');
+                assert.ok(moment, 'moment not loaded');
+                assert.ok(moment.twix, 'twix not loaded');
+                assert.ok(time, 'time not loaded');
+                assert.ok(bible, 'bible not loaded');
+                assert.ok(Spinner, 'Spinner not loaded');
+                assert.ok(ics, 'ics not loaded');
             });
         });
     });
