7d4359c7a0815aa0e07db26e34dce58f38855781
Kyle Hornberg <kyle.hornberg@gmail.com>
2013-12-16 11:13:57 -0600

    get descriptions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,4 @@
-/* global bible */
-/* global Spinner */
-/* global planner */
-/* global saveAs */
-/* global moment */
-/* global ics */
+/* global bible, Spinner, planner, saveAs, moment, ics */
 
 'use strict';
 
@@ -52,11 +47,35 @@ function getSkippedDays () {
 
         // select functionality
         $('.list-group-item').click(function () {
+            // TODO better way of doing this?          
             $('.list-group-item').each(function () {
                 $(this).removeClass('active');
             });
 
+            
+            var x = null;
+            $('.list-group-item').each(function () {
+                // Load text
+                planner.sequenceName = $(this).attr('name');
+                planner.load();
+
+                $('.panel-heading').text(planner.sequence.name);
+                $('.panel-body').html(planner.sequence.info);
+
+                x += $('.panel').html();
+
+            
+                console.info(x);
+            });
+
             $(this).addClass('active');
+
+            // Load text
+            planner.sequenceName = $('.list-group-item.active').attr('name');
+            planner.load();
+
+            $('.panel-heading').text(planner.sequence.name);
+            $('.panel-body').html(planner.sequence.info);
         });
 //     });
 // }
